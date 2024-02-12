3bc3f1a6db67e5bcdd46772138fb02d3bb9781d5
Marcel Frey <marcelfrey29@mailbox.org>
2024-02-12 17:34:45 +0100

    feat(rust): add `leptosfmt`

--- rust/cargo-install.sh
+++ rust/cargo-install.sh
@@ -5,5 +5,6 @@ echo "Building and installing Rust binaries..."
 cargo install diesel_cli --no-default-features --features postgres # Only install Postgres 
 cargo install trunk
 cargo install wasm-bindgen-cli # For "trunk": Required for Apple M1
+cargo install leptosfmt # To format `view!` macros in Leptos-Projects
 
 echo "Installed Rust binaries."
