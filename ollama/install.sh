d836d4f9757992ed06f999486c66b4713ebf89d7
Marcel Frey <marcelfrey29@mailbox.org>
2026-04-11 10:35:28 +0000

    feat(ollama): support Ollama in VS Code and GitHub Copilot, remove Continue, and install new models

--- ollama/install.sh
+++ ollama/install.sh
@@ -1,43 +1,28 @@
 #!/usr/bin/env bash
 
-# Config: Select Models
-EMBEDDINGS_MODEL="nomic-embed-text"
-CHAT_MODEL="llama3.1:8b"
-TAB_COMPLETION_MODEL="llama3.1:8b"
-
-# Download all models
 echo "Downloading Models..."
-ollama pull $EMBEDDINGS_MODEL
-ollama pull $CHAT_MODEL
-ollama pull $TAB_COMPLETION_MODEL
-echo "Downloaded Models."
 
-# Make sure the Continue Config exists so that editing it with yq is safe
-CONTINUE_CONFIG="$HOME/.continue/config.yaml"
-if [ ! -f "$CONTINUE_CONFIG" ]; then
-  touch $CONTINUE_CONFIG
-fi
+# Install Ollama Models
+# $1: array<string> the list of models to install
+function ollama_install_models() {
+    list=("$@")
+    for list_item in "${list[@]}"; do
+        echo "Installing \"${list_item}\"..."
+        ollama pull $list_item 
+        echo "Installed \"$list_item\"."
+    done
+}
 
-# General Config
-yq -i '.name = "Continue Config"' "$CONTINUE_CONFIG"
-yq -i '.version = "0.0.1"' "$CONTINUE_CONFIG"
+# Model List
+declare model_list=(
+    # Meta Llama 3 8b (8K Context, 4.7GB): https://ollama.com/library/llama3
+    "llama3.1:8b"
+    # Google DeepMind Gemma 4 31b (256K Context, 20GB): https://ollama.com/library/gemma4
+    # "gemma4:31b"
+    # Google DeepMind Gemma 4 e4b (128 Context, 9.6GB): https://ollama.com/library/gemma4
+    "gemma4:e4b"
+)
 
-# Embeddings Config
-yq -i '.models[0].name = "Ollama Continue Embeddings"' "$CONTINUE_CONFIG"
-yq -i '.models[0].provider = "ollama"' "$CONTINUE_CONFIG"
-yq -i ".models[0].model = \"$EMBEDDINGS_MODEL\"" "$CONTINUE_CONFIG"
-yq -i '.models[0].roles[0] = "embed"' "$CONTINUE_CONFIG"
+ollama_install_models "${model_list[@]}"
 
-# Chat Config
-yq -i '.models[1].name = "Ollama Continue Chat"' "$CONTINUE_CONFIG"
-yq -i '.models[1].provider = "ollama"' "$CONTINUE_CONFIG"
-yq -i ".models[1].model = \"$CHAT_MODEL\"" "$CONTINUE_CONFIG"
-yq -i '.models[1].roles[0] = "chat"' "$CONTINUE_CONFIG"
-
-# Tab-Completion Config
-yq -i '.models[2].name = "Ollama Continue Tab-Completion"' "$CONTINUE_CONFIG"
-yq -i '.models[2].provider = "ollama"' "$CONTINUE_CONFIG"
-yq -i ".models[2].model = \"$CHAT_MODEL\"" "$CONTINUE_CONFIG"
-yq -i '.models[2].roles[0] = "autocomplete"' "$CONTINUE_CONFIG"
-
-echo "Ollama and Continue are ready."
+echo "Models are downloaded."
